44dbf58084b31d7248a8f1c3bca635068245d0ca
Zard <32145741@qq.com>
2019-07-11 11:06:13 +0800

    Initial commit

---
+++ src/main/java/org/zado/springbootpoj/SpringbootpojApplication.java
@@ -0,0 +1,13 @@
+package org.zado.springbootpoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootpojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zado/springbootpoj/SpringbootpojApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zado.springbootpoj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootpojApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
